aeda73d0fdc26f6c0207d3433ca801c0afefcb53
Petr Daňhel <mrzutyoslik@users.noreply.github.com>
2016-12-07 14:28:16 +0100

    Fix of userscript property

--- sedm.user.js
+++ sedm.user.js
@@ -7,8 +7,8 @@
 // @include       http://www.sunitka.sk*
 // @author        Sun Marketing, s.r.o.  
 // @include       http://localhost:3000*
-// @downloadURL   https://raw.githubusercontent.com/SunMarketing/SEDM/master/sedm.js
-// @updateURL     https://raw.githubusercontent.com/SunMarketing/SEDM/master/sedm.js
+// @downloadURL   https://raw.githubusercontent.com/SunMarketing/SEDM/master/sedm.user.js
+// @updateURL     https://raw.githubusercontent.com/SunMarketing/SEDM/master/sedm.user.js
 // @icon          http://www.sunitka.cz/sedm/images/sedm.png
 // @description   SEDM je nástroj sloužící k analýze výsledků vyhledávaní fulltextových vyhledávačů Google a Seznam.cz. Za pomocí nástroje lze snadno získat data o hledanosti až padesáti klíčových slov ve vyhledávači Seznam.cz, počet nalezených výsledků na Seznam.cz i Google. Autory skriptu jsou Víťa Krchov, Matěj Velička a společnost Sun Marketing.
 // @version       0.37
